91c1cecbe61ea6944cc8a53296daf0ea57980d14
Juan Gonzalez <32402429+juanglzm@users.noreply.github.com>
2025-04-22 15:46:49 -0700

    new: disable emojis

--- includes/seo.php
+++ includes/seo.php
@@ -187,3 +187,60 @@ function zki_ht_preload_image_inside_block( array $block, int $preload_count = 0
 
 	return $preload_count;
 }
+
+/**
+ * Disable Emojis
+ *
+ * Disable anything related to wp-emoji
+ *
+ * @since 0.1.1
+ * @return void
+ */
+function zki_ht_disable_emojis() {
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+	add_filter( 'tiny_mce_plugins', 'zki_ht_disable_emojis_tinymce' );
+	add_filter( 'wp_resource_hints', 'zki_ht_disable_emojis_remove_dns_prefetch', 10, 2 );
+}
+add_action( 'init', 'zki_ht_disable_emojis' );
+
+/**
+ * Disable Emojis from TinyMCE
+ *
+ * Filter function used to remove the tinymce emoji plugin.
+ *
+ * @since 0.1.1
+ * @param array $plugins
+ * @return array Difference betwen the two arrays
+ */
+function zki_ht_disable_emojis_tinymce( array $plugins ) {
+	if ( is_array( $plugins ) ) {
+		return array_diff( $plugins, [ 'wpemoji' ] );
+	} else {
+		return [];
+	}
+}
+
+/**
+ * Remove Emojis DNS prefetch
+ *
+ * Remove emoji CDN hostname from DNS prefetching hints.
+ *
+ * @since 0.1.1
+ * @param array $urls URLs to print for resource hints.
+ * @param string $relation_type The relation type the URLs are printed for.
+ * @return array Difference betwen the two arrays.
+ */
+function zki_ht_disable_emojis_remove_dns_prefetch( array $urls, string $relation_type ) {
+	if ( 'dns-prefetch' == $relation_type ) {
+		$emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
+		$urls = array_diff( $urls, array( $emoji_svg_url ) );
+	}
+
+	return $urls;
+}
